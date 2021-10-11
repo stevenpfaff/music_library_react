71c594838420e74fd947cc26484fea5bebfbb1c1
stevenpfaff <pfaffy1426@gmail.com>
2021-10-11 11:02:21 -0500

    added Music Table Component and imported axios to app.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,18 +2,30 @@ import React, { Component } from "react";
 import './App.css'
 import TitleBar from "./TitleBar/TitleBar";
 import SearchBar from "./SearchBar/SearchBar";
+import axios from "axios";
+import MusicTable from "./MusicTable/MusicTable";
 
 class App extends Component {
-    constructor(props){
-        super(props);
-        this.state = {};
+    state = {
+        songs: []
     }
 
+    componentDidMount(){
+        this.getAllSongs();
+    }
+
+    async getAllSongs(){
+        let response = await axios.get('http://127.0.0.1:8000/music/');
+        this.setState({
+            songs: response.data
+        });
+    }
     render() {
         return (
             <div className="container-fluid">
             <TitleBar />
             <SearchBar />
+            <MusicTable />
             </div>
         )
     }
---
+++ src/components/MusicTable/MusicTable.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './MusicTable.css'
+
+function MusicTable(props){
+    return(
+        <div className='row row-space'>
+            <div className='col-md-12' style={{padding: 0}}>
+                <div className="musictable-nav">
+                <h1>Music Table</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default MusicTable;
